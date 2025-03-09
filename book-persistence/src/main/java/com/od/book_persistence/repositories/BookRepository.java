4b2ea611ee21af7cbe63914bbfef97bff7f6431d
Ostap Danyliv <danyliv.ostap@gmail.com>
2025-03-09 15:38:28 -0500

    Author persistence implemented

--- book-persistence/src/main/java/com/od/book_persistence/repositories/BookRepository.java
+++ book-persistence/src/main/java/com/od/book_persistence/repositories/BookRepository.java
@@ -3,6 +3,6 @@ package com.od.book_persistence.repositories;
 import com.od.book_persistence.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, String> {
 
 }
